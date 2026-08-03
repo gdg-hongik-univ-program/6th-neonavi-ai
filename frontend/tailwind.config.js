97fc09902c1825442582546f4ce2e412087fa338
xoaun <yoony7124@gmail.com>
2026-08-03 16:05:24 +0900

    S4, S5, S6, S7 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        // 브랜드 포인트 컬러 (와이어프레임 인디고)
+
         brand: '#5B5BD6',
       },
     },
